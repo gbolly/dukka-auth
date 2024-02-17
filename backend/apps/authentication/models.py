50c93e0742e7fb10b13af7029fb3d12b927c94c1
gbolly <gbolahan.okerayi@andela.com>
2024-02-17 03:00:49 +0100

    Update model definition

--- backend/apps/authentication/models.py
+++ backend/apps/authentication/models.py
@@ -1,15 +1,27 @@
-from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import AbstractBaseUser
+from django.db import models
 
 from .manager import UserManager
 
 
+def get_countries():
+    return {i: i for i in settings.COUNTRIES}
+
+
 class User(AbstractBaseUser):
+    GENDER_CHOICES = (
+        ('M', 'male'),
+        ('F', 'female'),
+        ('O', 'other'),
+    )
+    GENDER_MAPPER = {v: k for k, v in GENDER_CHOICES}
+
     full_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    sex = models.CharField(max_length=10, blank=True, null=True)
+    sex = models.CharField(max_length=1, choices=GENDER_CHOICES)
     phone_number = models.CharField(max_length=15, unique=True)
-    country = models.CharField(max_length=100, blank=True, null=True)
+    country = models.CharField(max_length=100, choices=get_countries)
     password = models.CharField(max_length=128)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
